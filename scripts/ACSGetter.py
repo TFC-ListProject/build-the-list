8b64b3dbce91b10cda7253e9314b9632cbf93560
mdeland <mdeland@box.com>
2017-04-01 19:59:55 -0700

    move state config to it's own file

--- scripts/ACSGetter.py
+++ scripts/ACSGetter.py
@@ -16,10 +16,12 @@ import csv
 import os
 import logging
 import sys
+import state_config
 
 def main():
     # list of state FIPS codes
-    state_codes = ['01','02','04','05','06','08','09','10','11','12','13','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41','42','44','45','46','47','48','49','50','51','53','54','55','56']
+    state_codes = {k: v['numeric_code'] for k, v in state_config.state_fips_codes.items()
+                   if v['status'] == 'State'}
 
     # list of get variables
     # TODO: rework to handle variables over 50 var max
@@ -40,8 +42,9 @@ def main():
 
     # loop through levels and states
     for hierarchy in for_hierarchies:
-        for state in state_codes:
-            get_acs(acs_key, get_string, hierarchy, state, url, prep, s)
+        for state, state_code in state_codes.items():
+            logging.info('running:%s', state)
+            get_acs(acs_key, get_string, hierarchy, state_code, url, prep, s)
 
 # set up session
 def make_session(url):
@@ -84,4 +87,5 @@ def get_acs(acs_key, get_string, for_hierarchy, state_code, url, prep, s):
     except: pass
 
 if __name__ == '__main__':
+    logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
     main()
