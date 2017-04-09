1bf2ba676a078743750897d2bc5a867869d04e18
Danny Olson <dbolson@gmail.com>
2017-04-09 14:25:23 -0700

    Can save list of candidates

---
+++ tests/test_db.py
@@ -0,0 +1,28 @@
+import build_the_list.db as db
+import conftest
+import records
+
+DB = records.Database('postgres://localhost:5432/buildthelist_test?user=techforcampaigns')
+
+def test_insert_candidates_sql():
+    candidates = ['c 1', 'c 2', 'c 3 4']
+    expected = """
+    INSERT INTO candidates (first_name, last_name) VALUES
+    ('c', '1'), ('c', '2'), ('c', '3 4')
+    RETURNING id
+    """
+    result = db.insert_candidates_sql(candidates)
+
+    assert result == expected
+
+def test_insert_candidates():
+    DB.query('TRUNCATE candidates RESTART IDENTITY CASCADE')
+    candidates = ['c 1', 'c 2', 'c 3 4']
+    expected = [
+        { 'id': 1 },
+        { 'id': 2 },
+        { 'id': 3 },
+    ]
+    result = db.insert_candidates(DB, candidates)
+
+    assert result == expected
